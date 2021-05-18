b46c88651095c51892a3d768f1308514326a44ec
i4n-v <ianvinicioss@gmail.com>
2021-05-18 15:20:18 -0300

    GH-31 -modification the validation mechanism

--- forms/patientForm.php
+++ forms/patientForm.php
@@ -1,20 +1,21 @@
-<?php 
-
-$error=$_GET['erro'];
-
- ?>
-
-
 <!DOCTYPE html>
 <html>
 <head>
 	<title>Registro do paciente</title>
 </head>
+<?php 
+
+$error=$_GET['erro'];
+
+ ?>
 <body>
 <form method="POST" action="../php/patientRegister.php">
-		<label>Nome:<input type="text" name="patientname" placeholder="Nome" minlength=3 maxlength=45></label>
+		<label>Nome:<input type="text" name="name" placeholder="Nome" minlength=3 maxlength=45></label>
 		<label>E-mail:<input type="email" name="email" placeholder="example@email.com"></label>
 		<label>CPF:<input type="text" name="cpf" placeholder="000.000.000-00" minlength=11 maxlength=11></label>
+		<label>Data de nascimento:<input type="date" name="date"></label>
+		<label>Peso:<input type="text" name="wheigth"></label>
+		<label>Altura:<input type="text" name="heigth"></label>
 		<label>Senha:<input type="password" name="password" placeholder="Senha" minlength=8></label>
 		<label>Confirmar Senha:<input type="password" name="confirmPW" placeholder="Confirmar Senha" minlength=8></label>
 		<input type="submit" name="Confirmar">
--- php/patientRegister.php
+++ php/patientRegister.php
@@ -4,15 +4,18 @@ require_once('../conect.php');
 $name = $_POST['name'];
 $cpf = $_POST['cpf'];
 $email = $_POST['email'];
+$date = $_POST['date'];
+$wheigth = floatval(trim(str_replace(',', '.', $_POST['wheigth'])));
+$heigth = floatval(trim(str_replace(',', '.', $_POST['heigth'])));
 $password = trim(sha1($_POST['password']));
 $confirmPW = trim(sha1($_POST['confirmPW']));
 
-$stmt = $con->prepare("SELECT `REGISTER_NUMBER` FROM `NUTRITIONISTS` WHERE `CPF`=?");
-$stmt->execute([$cpf]);
+$stmt = $con->prepare("SELECT `EMAIL`, `CPF` FROM `NUTRITIONISTS` WHERE (`CPF`=? OR `EMAIL`=?)");
+$stmt->execute([$cpf, $email]);
 $result = $stmt->fetch(PDO::FETCH_ASSOC);
 
-$stmt2 = $con->prepare("SELECT `EMAIL`, `CPF` FROM `PATIENTS` WHERE `CPF`=?");
-$stmt2->execute([$cpf]);
+$stmt = $con->prepare("SELECT `EMAIL`, `CPF` FROM `PATIENTS` WHERE (`CPF`=? OR `EMAIL`=?)");
+$stmt->execute([$cpf, $email]);
 $result2 = $stmt->fetch(PDO::FETCH_ASSOC);
 
 if ( $password != $confirmPW) {
@@ -20,25 +23,20 @@ if ( $password != $confirmPW) {
   header('location: ../forms/patientForm.php?erro=As senhas não coincidem');
   exit();
   
-  }else if($result['REGISTER_NUMBER']){
+  }else if($result2['EMAIL'] == $email || $result['EMAIL']){
 
-    header('location: ../forms/patientForm.php?erro=Usuário cadastrado como um nutricionista');
-    exit();
-
-  }else if($result2['EMAIL'] == $email){
-
-      header('location: ../forms/patientForm.php?erro=Email indisponivel');  
+      header('location: ../forms/patientForm.php?erro=E-mail ou CPF já cadastrado');  
       exit();
 
-    }  else if ($result2['CPF'] == $cpf) {
+    }else if ($result2['CPF'] == $cpf || $result['CPF']) {
         
-header('location: ../forms/patientForm.php?erro=CPF inválido');
+header('location: ../forms/patientForm.php?erro=E-mail ou CPF já cadastrado');
 exit();
 
 }
 	
-	$stmt = $con->prepare('INSERT INTO PATIENTS(CPF, NAME, PASSWORD, EMAIL) VALUES (?, ?, ?, ?)');
-$stmt->execute([$cpf, $name, $password, $email]);
+$stmt = $con->prepare('INSERT INTO PATIENTS(CPF, NAME, PASSWORD, EMAIL) VALUES (?, ?, ?, ?)');
+$stmt->execute([$cpf, $name, $date, $wheigth, $heigth, $password, $email]);
 
 header('location: ../forms/patientForm.php?erro=Paciente cadastrado com sucesso!');
 exit();
