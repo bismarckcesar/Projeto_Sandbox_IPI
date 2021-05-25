cc03db388685fd0c94d0e5a476601241b0690e95
FabioMouradeFraga <fabiomouradefraga@gmail.com>
2021-05-25 19:20:53 -0300

    Changing page title login, nutritionist registration and patient registration

--- view/loginForm.php
+++ view/loginForm.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html lang="pt-br">
 <head>
-	<title>Registro do nutricionista</title>
+	<title>Sandbox - entre ou cadastre-se</title>
 	<link rel="shortcut icon" href="../public/img/fav-icon.ico" type="image/x-icon">
 
 	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous">
