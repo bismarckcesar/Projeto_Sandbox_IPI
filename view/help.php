900adc4150ba9784a375e283be90ef00c4a4f8bb
alowce <alicebrandt.c@gmail.com>
2021-05-26 01:04:33 -0300

    GH-41 - Creating page help

---
+++ view/help.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Sandbox - ajuda</title>
+	<link rel="shortcut icon" href="../public/img/fav-icon.ico" type="image/x-icon">
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous">
+	<link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css" rel="stylesheet"/>
+	<link href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap" rel="stylesheet"/>
+	<link href="https://cdnjs.cloudflare.com/ajax/libs/mdb-ui-kit/3.5.0/mdb.min.css" rel="stylesheet"/>
+	<link href="../public/css/personalized.css" rel="stylesheet"/>
+</head>
+<body class="back-img">
+	<main>
+		<div class="container div-registro pt-3 rounded bg-light text-center">
+			<img src="../public/img/icon.svg">
+			<h5 class="text-center text-dark">Converse conosco através do e-mail:</h5>
+			<p>sandbox.pep2.2021@gmail.com</p>
+			<img src="../public/img/contact.png" class="img-contact">
+			<p><a class="link-style" href="../index.php">Voltar para a página inicial</a></p>
+		</div>
+	</main>
+</body>
+</html>
