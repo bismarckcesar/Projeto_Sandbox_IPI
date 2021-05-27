4d4b9a5be732b70aeb1c8f6c64abcaa2ced69233
FabioMouradeFraga <fabiomouradefraga@gmail.com>
2021-05-27 20:33:55 -0300

    Adding footer

---
+++ view/footer.php
@@ -0,0 +1,7 @@
+<footer class="text-center bg-color-custom">
+  <!-- Copyright -->
+  <div class="text-center text-light p-3">
+    © 2021 !VaiDarCerto, Inc.
+  </div>
+  <!-- Copyright -->
+</footer>
