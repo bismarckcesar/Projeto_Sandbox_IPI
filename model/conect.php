f442f64c19f1f942933fa9e6ff55ab82b6b1e1cb
bismarckcesar <bismarcksport@gmail.com>
2021-05-27 00:17:40 -0300

    GH-30 -adding search bar dropdown of page home

--- model/conect.php
+++ model/conect.php
@@ -2,7 +2,7 @@
 
 $dsn= "mysql:dbname=NUTRITIONISTS_SYSTEM;localhost=127.0.0.1:3306";
 $db_user= "root";
-$db_pass= "Odeiobancodedados";
+$db_pass= "bc961207";
 $con= new PDO($dsn, $db_user, $db_pass);
 
 
